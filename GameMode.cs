70e002886f6343b0fe453432079189ea0df2441d
jim berry <jim@bigfattail.com>
2019-12-17 10:56:39 -0500

    removed BaseMode - app should implement IGameMode directly

--- GameMode.cs
+++ GameMode.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 
 namespace GameModeMgr 
 {
+	
 	public interface IGameMode
 	{			
 		void Setup(ModeManager mgr, IGameInstance gameInst);
@@ -14,37 +15,5 @@ namespace GameModeMgr
 		string ModeName();
 	};
 
-
-	public abstract class BaseGameMode : IGameMode
-	{	
-		protected Dictionary<string,dynamic> _cmdDispatch;  	
-		public ModeManager manager; 
-		public IGameInstance gameInst;
-			
-		public void Setup(ModeManager mgr, IGameInstance gInst = null)
-		{
-			// Called by manager before Start()
-			// Not virtual
-			// TODO: this should be the engine and not the modeMgr - but what IS an engine...
- 			_cmdDispatch = new Dictionary<string, dynamic>();
-			manager = mgr;
-			gameInst = gInst;
-
-		}
-
-		public virtual void Start( object param = null)	{}
-
-		public virtual void Loop(float frameSecs) {}
-
-		public virtual void Pause() {}
-		public virtual void Resume(string prevModeName, object prevModeResult) {}	
-		public virtual object End() { return null;}     
-
-        public virtual bool HandleCmd(object cmd)
-        {
-            return _cmdDispatch[cmd.GetType().Name](cmd);        
-        }	
-        public virtual string ModeName() => this.GetType().Name;		   	
-	};
 }
 
